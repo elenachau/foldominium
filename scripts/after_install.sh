02ce56de5fa4661866a3fa09998ab8774b766935
Marlo-Ong <moon.ongkingco@gmail.com>
2024-07-29 21:31:16 -0700

    Update bash scripts
    - CodeDeploy save website to root folder instead of /website
    - Log datetime for commands
    - Run npm build before restarting server

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/foldominium/website/deploy.log
+log_message() {
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] *$1" >> /home/ec2-user/foldominium/website/deploy.log
+}
 
-echo 'cd /home/ec2-user/foldominium/website' >> /home/ec2-user/foldominium/website/deploy.log
+log_message 'run after_install.sh: '
+
+log_message 'cd /home/ec2-user/foldominium/website'
 cd /home/ec2-user/foldominium/website >> /home/ec2-user/foldominium/website/deploy.log
 
-echo 'npm install' >> /home/ec2-user/foldominium/website/deploy.log 
+log_message 'npm install' 
 npm install >> /home/ec2-user/foldominium/website/deploy.log
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+log_message() {
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] *$1" >> /home/ec2-user/foldominium/website/deploy.log
+}
 
-echo 'run application_start.sh: ' >> /home/ec2-user/foldominium/website/deploy.log
+log_message 'run application_start.sh:' 
+
+log_message 'npm build'
+npm build >> /home/ec2-user/foldominium/website/deploy.log
+
 # foldominium is the name as stored in pm2 process
-echo 'pm2 restart foldominium' >> /home/ec2-user/foldominium/website/deploy.log
+log_message 'pm2 restart foldominium'
 pm2 restart foldominium >> /home/ec2-user/foldominium/website/deploy.log
