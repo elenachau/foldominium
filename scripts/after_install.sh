d106b1f28acb72cbff3e3ee8f16a2cf147c567ce
Marlo-Ong <moon.ongkingco@gmail.com>
2024-07-31 19:55:24 -0700

    Test visual change and log formatting

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 log_message() {
-    echo "[$(date +'%Y-%m-%d %H:%M:%S')] *$1" >> /home/ec2-user/foldominium/website/deploy.log
+    printf "\n[%s] *%s" "$(date +'%Y-%m-%d %H:%M:%S')" "$1" >> /home/ec2-user/foldominium/website/deploy.log
 }
 
+printf "\n========================================"
 echo 'run after_install.sh: ' >> /home/ec2-user/foldominium/website/deploy.log
 
 log_message 'cd /home/ec2-user/foldominium/website'
@@ -10,3 +11,4 @@ cd /home/ec2-user/foldominium/website >> /home/ec2-user/foldominium/website/depl
 
 log_message 'npm install' 
 npm install >> /home/ec2-user/foldominium/website/deploy.log
+printf "\n========================================"
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 log_message() {
-    echo "[$(date +'%Y-%m-%d %H:%M:%S')] *$1" >> /home/ec2-user/foldominium/website/deploy.log
+    printf "\n[%s] *%s" "$(date +'%Y-%m-%d %H:%M:%S')" "$1" >> /home/ec2-user/foldominium/website/deploy.log
 }
 
+printf "\n========================================"
 echo 'run application_start.sh:' >> /home/ec2-user/foldominium/website/deploy.log
 
 log_message 'cd /home/ec2-user/foldominium/website'
@@ -12,8 +13,7 @@ log_message 'npm run build'
 sudo npm run build >> /home/ec2-user/foldominium/website/deploy.log 2>&1
 
 # foldominium is the name as stored in pm2 process
-log_message 'pm2 restart foldominium'
+log_message 'sudo pm2 restart foldominium'
 sudo pm2 restart foldominium >> /home/ec2-user/foldominium/website/deploy.log
 
-current_directory=$(pwd)
-echo "Current directory is: $current_directory"
+printf "\n========================================"
