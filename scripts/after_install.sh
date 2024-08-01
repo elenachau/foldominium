74bbee7986501a99f34796e1adbcc475c60dfdc1
Marlo-Ong <moon.ongkingco@gmail.com>
2024-07-31 19:44:23 -0700

    Test a fix to a CI/CD bash script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,7 +3,7 @@ log_message() {
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] *$1" >> /home/ec2-user/foldominium/website/deploy.log
 }
 
-log_message 'run after_install.sh: '
+echo 'run after_install.sh: ' >> /home/ec2-user/foldominium/website/deploy.log
 
 log_message 'cd /home/ec2-user/foldominium/website'
 cd /home/ec2-user/foldominium/website >> /home/ec2-user/foldominium/website/deploy.log
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,11 +3,17 @@ log_message() {
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] *$1" >> /home/ec2-user/foldominium/website/deploy.log
 }
 
-log_message 'run application_start.sh:' 
+echo 'run application_start.sh:' >> /home/ec2-user/foldominium/website/deploy.log
+
+log_message 'cd /home/ec2-user/foldominium/website'
+cd /home/ec2-user/foldominium/website >> /home/ec2-user/foldominium/website/deploy.log
 
 log_message 'npm run build'
-npm run build >> /home/ec2-user/foldominium/website/deploy.log
+sudo npm run build >> /home/ec2-user/foldominium/website/deploy.log 2>&1
 
 # foldominium is the name as stored in pm2 process
 log_message 'pm2 restart foldominium'
-pm2 restart foldominium >> /home/ec2-user/foldominium/website/deploy.log
+sudo pm2 restart foldominium >> /home/ec2-user/foldominium/website/deploy.log
+
+current_directory=$(pwd)
+echo "Current directory is: $current_directory"
