3402be089e203e4e644778ca3d6da9d7c9e774be
Marlo-Ong <moon.ongkingco@gmail.com>
2024-08-23 15:40:38 -0700

    Add creator and simulator pages

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,15 +1,33 @@
 import classes from "./App.module.css"
-import { Form } from "./Form/Form"
 import Header from "./Header/Header"
+import CreatorScreen from "../public/CreatorScreen.png"
+import OrigamiSimulatorScreen from "../public/OrigamiSimulatorScreen.png"
 
 function App() {
   return (
     <div className={classes.appView}>
       <Header />
       <div className={classes.body}>
-        {Form("Input an object, mesh, or shape to generate its origami crease pattern:",
-              "(e.g. A dog with wings, a piano, or a small piece text!)"
-        )}
+        <div className={classes.innerBody}>
+          <div className={classes.buttonWithImg}>
+            <a href="../simulator/cp-editor/cpedit.html">
+              <button type="button">Create a Fold!</button>
+            </a>
+            <p>How it works: Create, experiment, and simulate origami folds from scratch using our intuitive FOLD editor.</p>
+            <img className={classes.homeImg} src={CreatorScreen}></img>
+          </div>
+
+          <div className={classes.divider}></div>
+
+          <div className={classes.buttonWithImg}>
+            <a href="../simulator/OrigamiSimulator/index.html">
+              <button type="button">Simulate a Fold!</button>
+            </a>
+            <p>How it works: Bring your folds to life 3D viewport instantly utilizing OrigamiSimulator.</p>
+            <img className={classes.homeImg} src={OrigamiSimulatorScreen}></img>
+          </div>
+
+        </div>
       </div>
     </div>
   )
